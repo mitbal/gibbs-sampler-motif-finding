fa599f4393d8da94eea8a93b0541f9a486674828
mitbal <meteora>
2014-01-22 01:02:29 +0100

    Initial commit

---
+++ sequence_generator.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 21:52:08 2014
+
+@author: mit
+"""
+
+# This script generate sequence with hidden word with certain generative model
+
+import numpy as np
+from numpy.random import rand
+from numpy.random import dirichlet
+from numpy.random import randint
+
