4a88359062ab60be7b61d797221ff2d397213017
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2014-01-22 05:31:04 +0100

    Added option to write the resulting sequence to file along the necessary information

--- sequence_generator.py
+++ sequence_generator.py
@@ -26,6 +26,7 @@ from numpy.random import rand
 from numpy.random import dirichlet
 from numpy.random import randint
 
+WRITE_TO_FILE = True
 
 ### The properties for the generated sequence
 
@@ -78,9 +79,35 @@ for i in xrange(K):
         sequences[i][start_pos+j] = sample(alphabet, theta[j])
 
 
-# Happy printing
-print 'theta', theta
-print 'word position', position
-print 'sequences'
-for seq in sequences:
-    print seq
+if WRITE_TO_FILE:
+    ### Store the generated sequence to file to be read later
+    filename = 'a.seq'
+    
+    f = open(filename, 'w')
+    
+    f.write(str(K)+'\n')
+    f.write(str(N)+'\n')
+    f.write(str(w)+'\n')
+    f.write(''.join(alphabet)+'\n')
+    
+    f.write(','.join(map(str, alpha_b)) +'\n')
+    f.write(','.join(map(str, alpha_w)) +'\n')
+    
+    for s in sequences:
+        f.write(','.join(map(str, s)) +'\n')
+    
+    f.write(','.join(map(str, position)) +'\n')
+    
+    f.close()
+    
+else:
+    print 'K', K
+    print 'N', N
+    print 'w', w
+    print 'alphabet', alphabet
+    print 'alpha_b', alpha_b
+    print 'alpha_w', alpha_w
+    
+    for s in sequences:
+        print s
+    print 'position', position
