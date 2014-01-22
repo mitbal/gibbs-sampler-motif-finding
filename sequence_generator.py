361fc34ee1ae280310103232b275fc05d2e2612a
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2014-01-22 02:12:34 +0100

    Finished implementing sequence generator

--- sequence_generator.py
+++ sequence_generator.py
@@ -29,15 +29,58 @@ from numpy.random import randint
 
 ### The properties for the generated sequence
 
-K = 3   # The number of sequence
-N = 12  # The length for each sequence
-w = 5   # The length of the magic word
+K = 10   # The number of sequence
+N = 15  # The length for each sequence
+w = 6   # The length of the magic word
 
 alphabet = ['A', 'C', 'T', 'G']         # The alphabet used in the sequence
 M = len(alphabet)                       # The number of alphabet used
 
 alpha_b = [1]*M             # The alpha parameter for dirichlet prior of background letter
-alpha_w = [2,5,3,4]         # The alpha parameter for dirichlet prior of hidden word
+alpha_w = [10,2,8,3]         # The alpha parameter for dirichlet prior of hidden word
 
 
+### Start the generator part
 
+def sample(alphabet, dist):
+    """ This method produce a new discrete sample list by alphabet with probability
+    distribution given by dist.
+    The length of alphabet and dist must be same."""
+    sampl = None
+    cum_dist = np.cumsum(dist)
+    r = rand()
+    for i in xrange(len(dist)):
+        if r < cum_dist[i]:
+            sampl = alphabet[i]
+            break
+    
+    return sampl
+
+# First, generate the starting position of the magic word for all sequences uniformly
+position = [0]*K
+for i in xrange(K):
+    position[i] = randint(0, N-w+1)
+    
+# Generate the background letters for all sequences
+cat_b = dirichlet(alpha_b)
+sequences = []
+for i in xrange(K):
+    seq = []
+    for j in xrange(N):
+        seq += [sample(alphabet, cat_b)]
+    sequences += [seq]
+
+# Generate the magic words
+theta = dirichlet(alpha_w, w)
+for i in xrange(K):
+    start_pos = position[i]
+    for j in xrange(w):
+        sequences[i][start_pos+j] = sample(alphabet, theta[j])
+
+
+# Happy printing
+print 'theta', theta
+print 'word position', position
+print 'sequences'
+for seq in sequences:
+    print seq
